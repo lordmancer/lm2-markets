f1cac169fefa13718e487623d741a6105afe160c
Vasiliy Vazhesov <vasiliy.vazhesov@gmail.com>
2017-12-26 00:29:43 -0500

    New cities were added in config

--- bundle.js
+++ bundle.js
@@ -17032,16 +17032,7 @@ module.exports = getActiveElement;
 
 var config = {
     apiUrl: '//master.lordmancer2.com',
-    locationAndCities: [{
-        locationId: "woodlands",
-        cityId: "City1"
-    }, {
-        locationId: "greatgrove",
-        cityId: "City1"
-    }, {
-        locationId: "doomsmarsh",
-        cityId: "City1"
-    }],
+    locationAndCities: [{ locationId: "woodlands", cityId: "City1" }, { locationId: "greatgrove", cityId: "City1" }, { locationId: "doomsmarsh", cityId: "City1" }, { locationId: "cursedsands", cityId: "City1" }, { locationId: "bleaklake", cityId: "City1" }, { locationId: "sunnyisland", cityId: "City0" }, { locationId: "sunnyisland", cityId: "City1" }, { locationId: "sunnyisland", cityId: "City2" }],
     regions: ["en", "ru"]
 };
 
--- config.js
+++ config.js
@@ -1,18 +1,14 @@
 const config = {
     apiUrl: '//master.lordmancer2.com',
     locationAndCities: [
-        {
-            locationId: "woodlands",
-            cityId: "City1"
-        },
-        {
-            locationId: "greatgrove",
-            cityId: "City1"
-        },
-        {
-            locationId: "doomsmarsh",
-            cityId: "City1"
-        }
+        { locationId: "woodlands", cityId: "City1" },
+        { locationId: "greatgrove", cityId: "City1" },
+        { locationId: "doomsmarsh", cityId: "City1" },
+        { locationId: "cursedsands", cityId: "City1" },
+        { locationId: "bleaklake", cityId: "City1" },
+        { locationId: "sunnyisland", cityId: "City0" },
+        { locationId: "sunnyisland", cityId: "City1" },
+        { locationId: "sunnyisland", cityId: "City2" }
     ],
     regions: [
         "en",
